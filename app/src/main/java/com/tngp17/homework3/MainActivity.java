ef7a1768565e7be46812061f295873ca64156df0
Jane <jane.kansiree@gmail.com>
2017-08-14 22:26:24 +0700

    second commit

--- app/src/main/java/com/tngp17/homework3/MainActivity.java
+++ app/src/main/java/com/tngp17/homework3/MainActivity.java
@@ -1,7 +1,12 @@
 package com.tngp17.homework3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+
+        ImageButton ibPlay = (ImageButton) findViewById(R.id.ibPlay);
+
+        ibPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText.getText().length() != 0){
+                    Intent intent = new Intent(MainActivity.this, TapActivity.class);
+                    intent.putExtra("tapNo", editText.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/tngp17/homework3/TapActivity.java
@@ -0,0 +1,47 @@
+package com.tngp17.homework3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by TNGP17-001 on 14-Aug-17.
+ */
+
+public class TapActivity extends AppCompatActivity {
+    String tapNo = "";
+    int count = 0;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap);
+
+        final TextView tvTap = (TextView) findViewById(R.id.tvTap);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            tapNo = bundle.getString("tapNo");
+
+//            tvTap.setText(tapNo);
+        }
+        tvTap.setText(tapNo);
+
+        ImageButton ibCoin = (ImageButton) findViewById(R.id.ibCoin);
+
+        ibCoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if(Integer.parseInt(tapNo) != count){
+//                    count++;
+//                    tvTap.setText(count);
+//                }else{
+//                    finish();
+//                }
+            }
+        });
+    }
+}
